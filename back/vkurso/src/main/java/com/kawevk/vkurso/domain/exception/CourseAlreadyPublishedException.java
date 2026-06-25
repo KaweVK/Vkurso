43bac45ccff6d671176316734c56efcfccec1af4
KaweVK <victorkawe07@gmail.com>
2026-06-25 13:42:57 -0300

    Feat: add exception

---
+++ back/vkurso/src/main/java/com/kawevk/vkurso/domain/exception/CourseAlreadyPublishedException.java
@@ -0,0 +1,6 @@
+package com.kawevk.vkurso.domain.exception;
+
+public class CourseAlreadyPublishedException extends RuntimeException {
+    public CourseAlreadyPublishedException(long id) {
+    }
+}
