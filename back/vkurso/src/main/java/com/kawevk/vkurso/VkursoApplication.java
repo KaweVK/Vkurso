556f96a8ea02a0e80d5b18e032d48de95caccb00
KaweVK <victorkawe07@gmail.com>
2026-06-24 21:29:28 -0300

    First: Primeiro commit do back-end

---
+++ back/vkurso/src/main/java/com/kawevk/vkurso/VkursoApplication.java
@@ -0,0 +1,13 @@
+package com.kawevk.vkurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkursoApplication.class, args);
+	}
+
+}
