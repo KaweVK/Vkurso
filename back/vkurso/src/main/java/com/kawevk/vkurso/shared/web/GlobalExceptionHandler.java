7a31c6f64984f69279613dab148444130c0c3252
KaweVK <victorkawe07@gmail.com>
2026-06-27 00:26:57 -0300

    Feat(Handler): Adicionado novas exceções ao exception handler

--- back/vkurso/src/main/java/com/kawevk/vkurso/shared/web/GlobalExceptionHandler.java
+++ back/vkurso/src/main/java/com/kawevk/vkurso/shared/web/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.kawevk.vkurso.shared.web;
 
 import com.kawevk.vkurso.course.exceptions.CourseNotFoundException;
 import com.kawevk.vkurso.course.exceptions.DuplicateSlugException;
+import com.kawevk.vkurso.lesson.exceptions.LessonNotFoundException;
+import com.kawevk.vkurso.module.exceptions.ModuleNotFoundException;
+import com.kawevk.vkurso.shared.storage.StorageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +21,21 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DuplicateSlugException.class)
     public ProblemDetail handleDuplicate(DuplicateSlugException ex) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+    @ExceptionHandler(ModuleNotFoundException.class)
+    public ProblemDetail handleModuleNotFound(ModuleNotFoundException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(LessonNotFoundException.class)
+    public ProblemDetail handleLessonNotFound(LessonNotFoundException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(StorageException.class)
+    public ProblemDetail handleStorageException(StorageException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
 
     @ExceptionHandler(IllegalStateException.class)
