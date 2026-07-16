873a4b217f555c7b89ed1e7839093e4e5be3b259
KaweVK <victorkawe07@gmail.com>
2026-07-16 13:17:13 -0300

    Feat(Home): Usando hook de course no home

--- front/src/pages/home/index.tsx
+++ front/src/pages/home/index.tsx
@@ -1,30 +1,52 @@
 import '../../index.css'
 import Navbar from '../../components/navbar'
-import type { Course } from '../../types/course'
 import CardCurso from '../../components/card-curso';
+import { Link } from 'react-router-dom';
+import useCourses from '../../hooks/useCourse';
 
-const course: Course = {
-  id: 1,
-  title: "Java Spring Boot",
-  description: "Curso completo",
-  slug: "java-spring-boot",
-  price: 99.9,
-  level: "BEGINNER",
-  categoryIds: [1, 2],
-  status: "DRAFT",
-  instructorId: 10,
-  modules: [],
-};
 
 function Home() {
+    const { courses, loading } = useCourses();
+
+    if (loading) {
+        return (
+            <>
+                <Navbar />
+                <div className='flex bg-indigo-100 dark:bg-indigo-950 h-screen w-full items-center justify-center gap-8'>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6 animate-spin"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                    >
+                        <circle
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            color='white'
+                            stroke="currentColor"
+                            strokeWidth="3"
+                            strokeLinecap="round"
+                            strokeDasharray="60"
+                            strokeDashoffset="20"
+                        />
+                    </svg>
+                </div>
+            </>
+        );
+    }
+
     return (
         <>
-            <Navbar/>
-            <div className='flex bg-indigo-100 dark:bg-indigo-950 h-screen w-full items-center justify-center'>
-                <CardCurso course={course}/>
+            <Navbar />
+            <div className='flex bg-indigo-100 dark:bg-indigo-950 h-screen w-full items-center justify-center gap-8'>
+                {courses.map(course =>
+                    <Link to={`/course/${course.id}`}>
+                        <CardCurso course={course} />
+                    </Link>
+                )}
             </div>
         </>
-    
     )
 }
 
