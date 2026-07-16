043820462503937f27e5c8c6392771c65b15b36c
KaweVK <victorkawe07@gmail.com>
2026-07-16 16:26:47 -0300

    Feat(useCourses): add novo método para slug

--- front/src/hooks/useCourses.ts
+++ front/src/hooks/useCourses.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import courseService from "../services/courseService";
 import type { Course } from "../types/course";
 
-function useCourses() {
+export function useCourses() {
     const [courses, setCourses] = useState<Course[]>([]);
     const [loading, setLoading] = useState(true);
 
@@ -23,6 +23,28 @@ function useCourses() {
     }, []);
 
     return { courses, loading };
+}
+
+export function useCourse(slug?: string) {
+    const [course, setCourse] = useState<Course>();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function load() {
+            try {
+                const data = await courseService.findBySlug(slug);
+                setCourse(data);
+            } catch (err){
+                console.log(err)
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        load();
+    }, [slug]);
+
+    return { course, loading };
 }
 
 export default useCourses;
