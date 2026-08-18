0697583fb1b6033743768e35b1961a71fb00fb41
kawevk <victorkawe07@gmail.com>
2026-08-18 17:00:29 -0300

    fix(cache): corrigindo configuração do redis

--- back/vkurso/src/main/java/com/kawevk/vkurso/shared/config/RedisConfig.java
+++ back/vkurso/src/main/java/com/kawevk/vkurso/shared/config/RedisConfig.java
@@ -1,9 +1,14 @@
 package com.kawevk.vkurso.shared.config;
 
+import com.kawevk.vkurso.course.dtos.CourseResponse;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
@@ -12,9 +17,17 @@ import java.time.Duration;
 public class RedisConfig {
 
     @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-        return RedisCacheConfiguration.defaultCacheConfig()
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration courseCache = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
-                .disableCachingNullValues();
+                .disableCachingNullValues().serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new JacksonJsonRedisSerializer<>(CourseResponse.class)
+                        )
+                );
+
+        return RedisCacheManager.builder(connectionFactory)
+                .withCacheConfiguration("courses", courseCache)
+                .build();
     }
 }
