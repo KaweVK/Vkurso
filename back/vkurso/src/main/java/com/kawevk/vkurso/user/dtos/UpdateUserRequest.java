9e611eba4ea1d397e0237be473e6167ec8e1ce07
kawevk <victorkawe07@gmail.com>
2026-09-21 19:10:08 -0300

    Fix(course-category): corrige erro de verificação da categoria

--- back/vkurso/src/main/java/com/kawevk/vkurso/user/dtos/UpdateUserRequest.java
+++ back/vkurso/src/main/java/com/kawevk/vkurso/user/dtos/UpdateUserRequest.java
@@ -3,6 +3,7 @@ package com.kawevk.vkurso.user.dtos;
 import com.kawevk.vkurso.user.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UpdateUserRequest(
@@ -11,9 +12,7 @@ public record UpdateUserRequest(
         @NotBlank
         @Email @Size(max = 100)
         String email,
-        @NotBlank
-        String password,
-        @NotBlank @Size(max = 20)
+        @NotNull
         Role role
 ) {
 }
